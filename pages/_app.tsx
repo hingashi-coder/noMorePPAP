39dcc810d830cfb89a93586c927a3daa4973d06a
hingashi-coder <minecraft.kamata2020@gmail.com>
2022-03-07 22:50:30 +0900

    GA導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Layout} from "../components/Layout";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
+import * as gtag from '../lib/gtag'
 function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
       <>
+
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,41 @@
 import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
+  Html,
+  Head,
+  Main,
+  NextScript,
 } from "next/document";
-
+import { GA_TRACKING_ID } from '../lib/gtag'
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <title>No More PPAP</title>
-                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang={"ja"}>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <title>No More PPAP</title>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
+                rel="stylesheet"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+    );
+  }
 }
